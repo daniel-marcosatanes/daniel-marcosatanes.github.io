d5c1e8f5db869fcba418c1b4229ced102ea11950
daniel-marcosatanes <dani.marcos.atanes@gmail.com>
2025-06-12 22:01:52 +0000

    Deploying to gh-pages from @ daniel-marcosatanes/daniel-marcosatanes.github.io@150841f1b01ecc1ac996d208b7438b23feda3815 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
           window.open("mailto:%64%6D%61%72%61%74%61%6E%65%73@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
